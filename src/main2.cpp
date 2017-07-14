baf75fb05aebccf55ba69da22be78ae7e6f6c058
XTT <meteorologytoday@gmail.com>
2017-07-14 13:45:09 +0800

    Finish primary part

--- src/main2.cpp
+++ src/main2.cpp
@@ -27,31 +27,36 @@ typedef float T;
 const T kappa = 2.0 / 7.0;
 const T cp_over_cv = 7.0 / 3.0;
 const T R_ps_kappa = 287.0 / pow(101300, 2.0 / 7.0);
+const T g0 = 9.8;
+
 
 const int xpts = 256;
 const int zpts = 256;
 const int pts = xpts * zpts;
 
-const T Lx = 600000.0f;
-const T Lz = 600000.0f;
+const T Lx = 10000.0f;
+const T Lz = 10000.0f;
 const T nu = 0.1;
+const T theta_mean = 300.0f;
+
+
 T dx = Lx / (xpts - 1);
 T dz = Lz / (zpts - 1);
 
-T *zeta, *theta, *rho, *psi, *u, *w, *gdx_zeta, *gdz_zeta, *gdx_rho, *gdz_rho, \
-	*gdx_theta, *gdz_theta, *lap_zeta, *PGF_x, *PGF_z, *torq_1, *torq_2, *total_torq, *pre, \
-	*zeta_k1,  *zeta_k2,  *zeta_k3,  *zeta_k4, \
-	*theta_k1, *theta_k2, *theta_k3, *theta_k4, \
-	*rho_k1,   *rho_k2,   *rho_k3,   *rho_k4, \
-	
+T *zeta, *theta, *rho, *psi, *u, *w, *gdx_zeta, *gdz_zeta, \
+	*gdx_theta, *gdz_theta, *lap_zeta, *torq,          \
+	*zeta_k1,  *zeta_k2,  *zeta_k3,  *zeta_k4,         \
+	*theta_k1, *theta_k2, *theta_k3, *theta_k4,        \
 	*tmp, *zeta_tmp, *rho_tmp, *theta_tmp;
 
-const int total_steps = 1000;
-const int record_step = 10;
+const int total_steps = 3000;
+const int record_step = 100;
 const T dt = 3;
 
+
+
 const int max_iter = 5000;
-const T max_err = 0.001;
+const T max_err = 0.01 * dx;
 
 inline int I(int i, int j) {
 	return j * xpts + i;
@@ -71,25 +76,17 @@ void init() {
 	theta      = new T [pts];
 	rho        = new T [pts];
 	psi        = new T [pts];
-	pre        = new T [pts];
 	u          = new T [pts];
 	w          = new T [pts];
 	gdx_zeta   = new T [pts];
 	gdz_zeta   = new T [pts];
-	gdx_rho    = new T [pts];
-	gdz_rho    = new T [pts];
 	gdx_theta  = new T [pts];
 	gdz_theta  = new T [pts];
 	lap_zeta   = new T [pts];
-	PGF_x      = new T [pts];
-	PGF_z      = new T [pts];
-	torq_1     = new T [pts];
-	torq_2     = new T [pts];
-	total_torq = new T [pts];
+	torq       = new T [pts];
 	tmp        = new T [pts];
 	zeta_tmp   = new T [pts];
 	theta_tmp  = new T [pts];
-	rho_tmp    = new T [pts];
 
 	// used for RK4	
 	zeta_k1    = new T [pts];
@@ -102,50 +99,34 @@ void init() {
 	theta_k3   = new T [pts];
 	theta_k4   = new T [pts];
 
-	rho_k1     = new T [pts];
-	rho_k2     = new T [pts];
-	rho_k3     = new T [pts];
-	rho_k4     = new T [pts];
-
-	// assign profile
-	for(int j = 0; j < zpts; ++j) {
+	// init field
+	float x, z, r2;
+	float sigma = 1000.0;
+	float constant = 3000.0 / sigma / pow(2.0 * 3.14, 0.5);
+	float cx = 5000.0, cz = 3000.0;
+	for(int j=0; j < zpts; ++j) {
+		z = dz * j;
 		for(int i=0; i < xpts; ++i) {
-			theta[I(i,j)] = 300.0f;
-			pre[I(i,j)] = 
+			x = dx * i;
+			r2 = pow(x - cx, 2.0) + pow(z - cz, 2.0);
+			
+			theta[I(i,j)] = constant * exp(- r2 / pow(sigma, 2.0)); 
 		}
 	}
 }
 
 
 
-void cal_change(T * zeta, T * theta, T * rho, T * zeta_change, T * theta_change, T * rho_change) {
-	// calculate pressure
-	for(int j = 0; j < zpts; ++j) {
-		for(int i = 0; i < xpts; ++i) {
-			pre[I(i,j)] = pow(rho[I(i,j)] * theta[I(i,j)] * R_ps_kappa, cp_over_cv);
-		}
-	}
-
-	// calculate PGF
-	gradx(pre, PGF_x);
-	gradz(pre, PGF_z);
-
-	for(int j = 0; j < zpts; ++j) {
-		for(int i = 0; i < xpts; ++i) {
-			PGF_x[I(i,j)] /= - rho[I(i,j)];
-			PGF_z[I(i,j)] /= - rho[I(i,j)];
-		}
-	}
+void cal_change(T * zeta, T * theta, T * zeta_change, T * theta_change) {
 
 	// calculate torq
-	gradz(PGF_x, torq_1);
-	gradx(PGF_z, torq_2);
-	for(int j = 0; j < zpts; ++j) {
-		for(int i = 0; i < xpts; ++i) {
-			total_torq[I(i,j)] = torq_1[I(i,j)] - torq_2[I(i,j)];
-		}
-	}
+	gradx(theta, torq);
 
+	for(int i = 0; i < pts; ++i) {
+		torq[i] *= g0 / theta_mean;
+	}
+	output_data(torq, "torq.bin");
+	
 	int flag = 0;
 	// invert wind from zeta
 	if((flag = jacobi_relaxation(zeta, psi, tmp, max_err, max_iter)) != 0) {
@@ -157,6 +138,8 @@ void cal_change(T * zeta, T * theta, T * rho, T * zeta_change, T * theta_change,
 	gradz(psi, u); for(int i =0; i < pts; ++i) { u[i] *= -1.0f; } 
 	gradx(psi, w);
 
+	output_data(u, "u.bin");
+	output_data(w, "w.bin");
 	// calculate grad vorticity
 	gradx(zeta, gdx_zeta);
 	gradz(zeta, gdz_zeta);
@@ -164,29 +147,17 @@ void cal_change(T * zeta, T * theta, T * rho, T * zeta_change, T * theta_change,
 	// calculate lap vort
 	laplacian(zeta, lap_zeta);
 
-	for(int j = 0; j < zpts; ++j) {
-		for(int i = 0; i < xpts; ++i) {
-			zeta_change[I(i,j)] = - u[I(i,j)] * gdx_zeta[I(i,j)] - w[I(i,j)] * gdz_zeta[I(i,j)] + nu * lap_zeta[I(i,j)] + total_torq[I(i,j)];
-		}
+	for(int i = 0; i < pts; ++i) {
+		zeta_change[i] = - u[i] * gdx_zeta[i] - w[i] * gdz_zeta[i] + nu * lap_zeta[i] + torq[i];
 	}
 	
 	// calculate theta part
 	gradx(theta, gdx_theta);
 	gradz(theta, gdz_theta);
-	for(int j = 0; j < zpts; ++j) {
-		for(int i = 0; i < xpts; ++i) {
-			theta_change[I(i,j)] = - u[I(i,j)] * gdx_theta[I(i,j)] - w[I(i,j)] * gdz_theta[I(i,j)];
-		}
+	for(int i = 0; i < pts; ++i) {
+		theta_change[i] = - u[i] * gdx_theta[i] - w[i] * gdz_theta[i];
 	}
-	
-	// calculate rho part
-	gradx(rho, gdx_rho);
-	gradz(rho, gdz_rho);
-	for(int j = 0; j < zpts; ++j) {
-		for(int i = 0; i < xpts; ++i) {
-			rho_change[I(i,j)] = - u[I(i,j)] * gdx_rho[I(i,j)] - w[I(i,j)] * gdz_rho[I(i,j)];
-		}
-	}
+	output_data(theta_change, "theta_change.bin");	
 }
 
 
@@ -203,43 +174,42 @@ int main() {
 		if(record_flag) {
 			sprintf(filename, "output/zeta_step_%d.bin", step);
 			output_data(zeta, filename);
+			
+			sprintf(filename, "output/theta_step_%d.bin", step);
+			output_data(theta, filename);
 		}
 
 		for(int rk = 0 ; rk < 4; ++rk) {
 
 			switch (rk) {
 				case 0:
-					cal_change(zeta, theta, rho, zeta_k1, theta_k1, rho_k1);
+					cal_change(zeta, theta, zeta_k1, theta_k1);
 					break;
 				case 1:
 					evolve(zeta,  zeta_k1,  dt/2.0, zeta_tmp);
 					evolve(theta, theta_k1, dt/2.0, theta_tmp);
-					evolve(rho,   rho_k1,   dt/2.0, rho_tmp);
 					
-					cal_change(zeta_tmp, theta_tmp, rho_tmp, zeta_k2, theta_k2, rho_k2);
+					cal_change(zeta_tmp, theta_tmp, zeta_k2, theta_k2);
 
 					break;
 				case 2:
 					evolve(zeta,  zeta_k2,  dt/2.0, zeta_tmp);
 					evolve(theta, theta_k2, dt/2.0, theta_tmp);
-					evolve(rho,   rho_k2,   dt/2.0, rho_tmp);
 					
-					cal_change(zeta_tmp, theta_tmp, rho_tmp, zeta_k3, theta_k3, rho_k3);
+					cal_change(zeta_tmp, theta_tmp, zeta_k3, theta_k3);
 					break;
 				case 3:
 					evolve(zeta,  zeta_k3,  dt/2.0, zeta_tmp);
 					evolve(theta, theta_k3, dt/2.0, theta_tmp);
-					evolve(rho,   rho_k3,   dt/2.0, rho_tmp);
 					
-					cal_change(zeta_tmp, theta_tmp, rho_tmp, zeta_k4, theta_k4, rho_k4);
+					cal_change(zeta_tmp, theta_tmp, zeta_k4, theta_k4);
 					break;
 			}
 		}
 
-		for(int j=0; j<zpts; ++j) {
-			for(int i=0; i<xpts; ++i) {
-				zeta[I(i,j)] += (zeta_k1[I(i, j)] + 2.0 * zeta_k2[I(i, j)] + 2.0 * zeta_k3[I(i, j)] + zeta_k4[I(i, j)]) / 6.0;
-			}
+		for(int i=0; i<pts; ++i) {
+			zeta[i] += (zeta_k1[i] + 2.0 * zeta_k2[i] + 2.0 * zeta_k3[i] + zeta_k4[i]) / 6.0;
+			theta[i] += (theta_k1[i] + 2.0 * theta_k2[i] + 2.0 * theta_k3[i] + theta_k4[i]) / 6.0;
 		}
 	}
 }
@@ -282,8 +252,8 @@ void evolve(T * f, T * df, T dt, T * o_f) {
 
 int jacobi_relaxation(T * input, T * output, T * tmp, T max_err, int max_iter) {
 	/*
-	 * The input cannot be protected from damaging.
-	 * Boundarz condition is specifz on the margin of input
+	 * Initial guess is stored in output
+	 * Boundary condition is specified on the margin of input
 	 */
 	T err = 0, r;
 	T gamma = 1 / (2 * ( 1/(dx*dx) + 1/(dz*dz) ) );
@@ -304,7 +274,7 @@ int jacobi_relaxation(T * input, T * output, T * tmp, T max_err, int max_iter) {
 		}
 
 		err /= avg_pts;
-		fprintf(stderr,"[%d] Err avg: %5f\n",iter, err);
+		fprintf(stderr,"[%d] Err avg: %e\n",iter, err);
 
 		if(err < max_err)
 			return 0;
@@ -343,83 +313,3 @@ int output_data(T * dat, const char * filename) {
 	fclose(file);
 	return 0;
 }
-/*
-int main() {
-
-	init();
-
-	int err;
-
-	auto evolution = [&] (ModelData& k, ModelData& evo_vort) {
-		vort_tmp2.copzData(evo_vort);
-		EnuData::each_index([&](xsize i, xsize j) {
-			vort_tmp2(i,j) *= bc_filter(i,j);
-		});
-
-		// inverse vort to psi
-		if((err = jacobi_relaxation(vort_tmp2, psi, tmp, dx, dz, max_err, max_iter)) != 0) {
-			fprintf(stderr, "[Error code %d]: Relaxation cannot converge within [%d] iterations.\n", err, max_iter);
-			return -1;
-		}
-
-		// calculate u,v
-		gradz(psi, u, dz); u *= -1.0f;
-		gradx(psi, v, dx);
-
-		// calculate grad vort
-		gradx(evo_vort, gradx_vort, dx);
-		gradz(evo_vort, gradz_vort, dz);
-
-		// calculate lap vort
-		lap(evo_vort, lap_vort, dx, dz);
-		
-		EnuData::each_index([&](xsize i, xsize j){
-			k(i,j) = -u(i,j) * gradx_vort(i,j) - v(i,j) * gradz_vort(i,j) + nu * lap_vort(i,j);	
-		});
-	
-	};
-
-	auto calculator = [&] (ModelData& output, ModelData& input, ModelData& k, T dt) {
-		EnuData:: each_index([&](xsize i, xsize j) {
-			output(i,j) = input(i,j) + k(i,j) * dt;		
-		});
-	};
-
-	for(xsize i = 0; i < step; ++i) {
-		cout << "step:" << i << endl;
-		evolution(k1, vort); 	 calculator(vort_tmp, vort, k1, 0.5 * dt);
-		evolution(k2, vort_tmp); calculator(vort_tmp, vort, k2, 0.5 * dt);
-		evolution(k3, vort_tmp); calculator(vort_tmp, vort, k3, dt);
-		evolution(k4, vort_tmp);
-
-		EnuData::each_index([&](xsize i, xsize j){
-			vort(i,j) +=  dt / 6.0 * (k1(i,j) + 2*k2(i,j) + 2*k3(i,j) + k4(i,j));
-		});
-
-
-		if(i % 10 == 0) {
-			stringstream filename;
-			filename << "vort_" << i << ".bin";
-			output_data(vort, filename.str().c_str());
-		}
-		// calculate new vort
-	}
-//	cout << "start" << endl;
-//	cout << "Jacobi Result: " << jacobi_relaxation(vort, psi, tmp, dx, dz, max_err, max_iter) << endl;
-	//lap(psi,vort,dx,dz);
-
-//	psi = vort;
-
-//	cout << &(psi.getData(0)) << " compare " << (psi.getDataPointer()) << endl;
-
-//	cout << "Seg?" <<endl;
-//	psi(0,0) += 200;
-
-
-//	output_data(vort, "init_vort.bin");
-//	output_data(psi, "init_psi.bin");
-
-	return 0;
-}
-
-*/
